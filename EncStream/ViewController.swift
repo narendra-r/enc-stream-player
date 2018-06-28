6aa0ce824acee6dd78a49253c3c1b0d0ff7a866b
Narendrakumar Rangisetty <narendra.r@outlook.com>
2018-06-28 21:58:21 +0530

    Initial Commit

---
+++ EncStream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EncStream
+//
+//  Created by Narendra Kumar R on 6/28/18.
+//  Copyright © 2018 Narendra Kumar R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
