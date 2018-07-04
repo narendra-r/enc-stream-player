dda1ff7734868231b5106dfd5a4c2a49abd091c6
Narendrakumar Rangisetty <narendra.r@outlook.com>
2018-07-04 07:23:55 +0530

    cleaned player, decryption not working.

--- EncStream/ViewController.swift
+++ EncStream/ViewController.swift
@@ -7,44 +7,76 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import AVKit
+class ViewController: UIViewController, DVAssetLoaderDelegatesDelegate {
     private weak var avPlayer: AVPlayer?
-    @IBOutlet weak var videoPlayerView: VideoPlayerView!
+    private var videoVC: AVPlayerViewController!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let plain_video = "https://s3.amazonaws.com/simple-naren/big_buck_bunny.mp4"
+//        let plain_video = "https://s3.amazonaws.com/simple-naren/big_buck_bunny.mp4"
         let enc_url = "https://s3.amazonaws.com/simple-naren/enc_earth_video.mp4"
-        playfrom(audioUrl: plain_video)
+        playfrom(videoURL: enc_url)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    private func playfrom(audioUrl: String) {
-        videoPlayerView.setupPlayerView(urlString : audioUrl)
-        self.avPlayer = videoPlayerView.player
+    
+    private func playfrom(videoURL: String) {
+        
+        if let url = URL(string: videoURL) {
+            let resourceLoaderDelegate = DVAssetLoaderDelegate.init(url: url)
+            resourceLoaderDelegate?.delegate = self;
+            
+            let components = NSURLComponents.init(url: url, resolvingAgainstBaseURL: false)
+            components?.scheme = DVAssetLoaderDelegate.scheme();
+            
+            let asset = AVURLAsset.init(url: (components?.url)!)
+            asset.resourceLoader.setDelegate(resourceLoaderDelegate, queue: .main)
+            
+            let assetKeys = ["playable","hasProtectedContent"]
+            let playerItem = AVPlayerItem(asset: asset,automaticallyLoadedAssetKeys: assetKeys)
+            avPlayer = AVPlayer.init(playerItem: playerItem)
+            
+            avPlayer?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying),
+                                                   name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: avPlayer!.currentItem)
+        }
+        
+        
+        videoVC = childViewControllers[0] as! AVPlayerViewController
+        videoVC.view.clipsToBounds = true
+        videoVC.player = avPlayer
+        videoVC.showsPlaybackControls = true
         
         avPlayer?.addPeriodicTimeObserver(forInterval: CMTimeMake(1, 2), queue: DispatchQueue.main, using: { (time) in
-            
-            if let videoItem = self.avPlayer?.currentItem {
-                let currentTime : Float64 = CMTimeGetSeconds(videoItem.currentTime());
-                let totalDuration : Float64 = CMTimeGetSeconds(videoItem.duration);
-                let progress = currentTime / totalDuration
-                self.videoPlayerView.videoSlider.value = Float(progress)
-                
-            }
+            // This need to have to update transcript timeline
         })
-        
+
         avPlayer?.addObserver(self, forKeyPath: "status", options: .initial, context: nil)
     }
+    @objc private func playerDidFinishPlaying(note: NSNotification) {
+        
+    }
+    
+    func dvAssetLoaderDelegate(_ loaderDelegate: DVAssetLoaderDelegate!, didLoad data: Data!, for url: URL!) {
+        print("finished")
+    }
+    func dvAssetLoaderDelegate(_ loaderDelegate: DVAssetLoaderDelegate!, didRecieveLoadingError error: Error!, with dataTask: URLSessionDataTask!, for request: AVAssetResourceLoadingRequest!) {
+        print("failed")
+    }
+    
+    func dvAssetLoaderDelegate(_ loaderDelegate: DVAssetLoaderDelegate!, didLoad data: Data!, for range: NSRange, url: URL!) {
+        print("data : \(data)")
+    }
+
+    
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
         if avPlayer == object as? AVPlayer, keyPath  == "status" {
